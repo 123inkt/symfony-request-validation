ec454275c0ad1a98fee7742eb8b009bfae3c1448
Bram Leeda <bram@123inkt.nl>
2021-09-20 16:37:47 +0200

    Add import that's used in PHPDoc only, but was auto removed by PHPStorm

--- src/ValidationRules.php
+++ src/ValidationRules.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 namespace DigitalRevolution\SymfonyRequestValidation;
 
 use InvalidArgumentException;
+use Symfony\Component\Validator\Constraint;
 
 class ValidationRules
 {
