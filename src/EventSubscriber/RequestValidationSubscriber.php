e9ab07f049844a9160d9e4292e8acbec280462e8
Frank Dekker <fdekker@123inkt.nl>
2022-08-26 16:09:28 +0200

    Add request validation bundle

--- src/EventSubscriber/RequestValidationSubscriber.php
+++ src/EventSubscriber/RequestValidationSubscriber.php
@@ -38,6 +38,7 @@ class RequestValidationSubscriber implements EventSubscriberInterface
 
             if ($result instanceof Response) {
                 $event->setController(fn() => $result);
+                break;
             }
         }
     }
