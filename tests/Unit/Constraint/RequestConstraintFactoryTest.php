88f4a0a2b398982ba208351b32f724619345b4fc
Frank Dekker <fdekker@123inkt.nl>
2022-08-26 16:15:58 +0200

    Add bundle test coverage

--- tests/Unit/Constraint/RequestConstraintFactoryTest.php
+++ tests/Unit/Constraint/RequestConstraintFactoryTest.php
@@ -1,7 +1,7 @@
 <?php
 declare(strict_types=1);
 
-namespace Constraint;
+namespace DigitalRevolution\SymfonyRequestValidation\Tests\Unit\Constraint;
 
 use DigitalRevolution\SymfonyRequestValidation\Constraint\RequestConstraint;
 use DigitalRevolution\SymfonyRequestValidation\Constraint\RequestConstraintFactory;
