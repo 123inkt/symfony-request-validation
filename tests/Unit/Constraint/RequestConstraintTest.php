a3a3dd36a87372fa698aeb03280dc6cc01c1e20f
Frank Dekker <fdekker@123inkt.nl>
2025-12-29 13:48:01 +0100

    Fix tests

--- tests/Unit/Constraint/RequestConstraintTest.php
+++ tests/Unit/Constraint/RequestConstraintTest.php
@@ -8,7 +8,6 @@ use PHPUnit\Framework\TestCase;
 use Symfony\Component\Validator\Constraints\IsNull;
 use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\Validator\Constraints\NotNull;
-use Symfony\Component\Validator\Exception\InvalidOptionsException;
 
 /**
  * @coversDefaultClass \DigitalRevolution\SymfonyRequestValidation\Constraint\RequestConstraint
@@ -35,30 +34,10 @@ class RequestConstraintTest extends TestCase
         $constraintA = new NotBlank();
         $constraintB = new NotNull();
         $constraintC = new IsNull();
-        $constraint  = new RequestConstraint(
-            ['query' => $constraintA, 'request' => $constraintB, 'attributes' => $constraintC, 'allowExtraFields' => true]
-        );
+        $constraint = new RequestConstraint($constraintA, $constraintB, $constraintC, true);
         static::assertSame($constraintA, $constraint->query);
         static::assertSame($constraintB, $constraint->request);
         static::assertSame($constraintC, $constraint->attributes);
         static::assertTrue($constraint->allowExtraFields);
-    }
-
-    /**
-     * @covers ::__construct
-     */
-    public function testConstructIncorrectOption(): void
-    {
-        $this->expectException(InvalidOptionsException::class);
-        new RequestConstraint(['invalid' => 'invalid']);
-    }
-
-    /**
-     * @covers ::getRequiredOptions
-     */
-    public function testGetRequiredOptions(): void
-    {
-        $constraint = new RequestConstraint();
-        static::assertSame(['query', 'request', 'attributes', 'allowExtraFields'], $constraint->getRequiredOptions());
     }
 }
